9507bcbc182d43e5b86849ad550b619dee8ad8ea
vivek880490 <54447269+vivek880490@users.noreply.github.com>
2021-12-19 20:58:38 +0530

    First commit

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const bodyParser = require('body-parser');
 
 //Import db module
 const db = require('./db.js');
+const MongoClient = require('mongodb').MongoClient;
+
+// replace the uri string with your connection string.
+const uri = "mongodb+srv://vivek880:vivek880@cluster0.q1r8u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+MongoClient.connect(uri, function (err, client) {
+    if (err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+    }
+    console.log('Connected...');
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 //Import controllers
 const {
